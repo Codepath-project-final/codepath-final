6c0bae61837d5c04addf548c19bbae9baaea8789
Jangey <jangey.lu@gmail.com>
2018-11-11 14:10:13 -0800

    connect detail view to database

--- Parq/Parq/Description Views/DescriptionViewController.swift
+++ Parq/Parq/Description Views/DescriptionViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class DescriptionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class DescriptionViewController: UIViewController{
 
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var messageButton: UIButton!
     @IBOutlet weak var bookNowButton: UIButton!
     @IBOutlet weak var photoTableView: UIImageView!
+    
+    var post : [String: Any]? = nil
     
     
     
@@ -22,19 +24,14 @@ class DescriptionViewController: UIViewController, UITableViewDataSource, UITabl
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-       
-    }
-    
+        if let post = post {
 
-    func tableView(_ photoTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // return photo.count
-        return 10
-    }
-    
-    func tableView(_ photoTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = photoTableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-        
-        return cell
+            descriptionLabel.text = post["post_description"] as! String?
+            
+            let imageUrl = post["imageURL"] as! String
+            let url = URL(string: imageUrl)!
+            photoTableView.af_setImage(withURL: url)
+        }
     }
     
    
--- Parq/Parq/Views/ViewController.swift
+++ Parq/Parq/Views/ViewController.swift
@@ -207,6 +207,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell){
+            let post = posts[indexPath.row]
+            let detailViewController = segue.destination as! DescriptionViewController
+            detailViewController.post = post
+        }
+        
+    }
     
 }
 
